0fc0c600a9251ade139e4b7516730bd8de47eed2
abombss <adam.tybor@gmail.com>
2011-01-29 02:41:25 -0600

    Adding isEmpty assertion to TextView for better readability

--- src/main/java/com/github/calculon/assertion/TextViewAssertion.java
+++ src/main/java/com/github/calculon/assertion/TextViewAssertion.java
@@ -1,11 +1,11 @@
 package com.github.calculon.assertion;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 import android.app.Activity;
 import android.test.InstrumentationTestCase;
 import android.test.MoreAsserts;
 import android.widget.TextView;
+
+import static junit.framework.Assert.*;
 
 public class TextViewAssertion extends ViewAssertion {
 
@@ -14,6 +14,14 @@ public class TextViewAssertion extends ViewAssertion {
     public TextViewAssertion(InstrumentationTestCase testCase, Activity activity, TextView textView) {
         super(testCase, activity, textView);
         this.textView = textView;
+    }
+
+    /**
+     * Asserts that the text within the textview is and empty String
+     */
+    public void isEmpty() {
+        String text = textView.getText().toString();
+        assertTrue("expected '" + text +  "' to be empty, but it wasn't", "".equals(text));
     }
 
     public void reads(String text) {
